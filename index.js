695e83930bcdfa54f11f0b7cddd42cf7422da04e
Alejandra Castillo <ahle.castillo@gmail.com>
2018-07-26 00:14:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"];
-function destructivdlyAppendDriver() {
+function destructivdlyAppendDriver(name) {
   return drivers.push('name');
 }
+function destructivelyPrependDriver(name)
