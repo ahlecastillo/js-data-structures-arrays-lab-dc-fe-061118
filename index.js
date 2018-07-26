8f11ad5db1f986cd9ea6cd191a1141262277428a
Alejandra Castillo <ahle.castillo@gmail.com>
2018-07-26 00:50:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function appendDriver(name){
 }
 
 function prependDriver(name) {
-  return ['name',...drivers];
+  return ['Arnold',...drivers];
 }
 function removeLastDriver() {
   return drivers.slice(0, drivers.length - 1);
